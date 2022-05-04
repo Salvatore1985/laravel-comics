2473244e7ebaeb4ccf4453e5557ec95ed3996a66
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-05-04 15:21:49 +0200

    add template

---
+++ config/menu.php
@@ -0,0 +1,59 @@
+<?php
+return [
+    [
+        "text" => "charactecrs",
+        "url" => "#",
+        "current" => true,
+    ],
+    [
+        "text" => "comics",
+        "url" => "#",
+        "current" => false,
+    ],
+    [
+        "text" => "movies",
+        "url" => "#",
+        "current" => true,
+    ],
+    [
+        "text" => "tv",
+        "url" => "#",
+        "current" => true,
+    ],
+    [
+        "text" => "games",
+        "url" => "#",
+        "current" => true,
+    ],
+    [
+        "text" => "collectivles",
+        "url" => "#",
+        "current" => true,
+    ],
+    [
+        "text" => "videos",
+        "url" => "#",
+        "current" => true,
+    ],
+    [
+        "text" => "fans",
+        "url" => "#",
+        "current" => true,
+    ],
+
+    [
+        "text" => "news",
+        "url" => "#",
+        "current" => true,
+    ],
+    [
+        "text" => "charactecrs",
+        "url" => "#",
+        "current" => true,
+    ],
+    [
+        "text" => "shop",
+        "url" => "#",
+        "current" => true,
+    ]
+];
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+    <div class="my-w-70">
+        <div class="header-content">
+          <div id="logo">
+            <img src="{{assets(/img/dc-logo.png" alt="logo)}}" />
+          </div>
+          <nav>
+            <ul class="my-nav-header">
+                
+                <li>
+
+                </li>
+            {{--   <li
+                v-for="(element, index) in navHeder"
+                :key="index"
+                :class="{ active: element.current }"
+              >
+                <a :href="element.url">{{ element.text }}</a>
+              </li> --}}
+            </ul>
+          </nav>
+        </div>
+      </div>
+</header>
