42ad70b650b9e3b75cc9d9511ee6cf9acb782559
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-05-05 20:58:27 +0200

    up info-cover

--- resources/views/guest/infocover.blade.php
+++ resources/views/guest/infocover.blade.php
@@ -4,19 +4,27 @@
 
 @section('main-content')
 <section class="cover-info">
-   
+    <div>
+        <figure>
+            <img src="{{$comic['thumb']}}" alt="">
+            <span class="text-top my-text-white my-text-transform-uppercase ">comic book</span>
+            <span class="text-bot my-text-white my-text-transform-uppercase ">view gallery</span>
+        </figure>
+    </div>
 
 </section>
 <section class="single-cover">
     <div class="my-d-flex my-h-100">
-        <div class="cover-title my-py-3 ">
+        <div class="cover-title my-py-2 ">
             <h1 class="my-py-2">{{$comic['title']}}</h1>
-            <div class="my-d-flex  border-b-1">
+            <div class="my-d-flex">
                 <div class="price-cover my-d-flex 
                             my-justify-content-between 
                             my-align-items-center
-                            my-px-1">
-                    <span class="my-text-green"> U.S Price: $<span class="my-text-white">{{$comic['price']}}</span></span>
+                            my-px-1
+                            border-b-1
+                           ">
+                    <span class="my-text-green"> U.S Price: <span class="my-text-white">{{$comic['price']}}</span></span>
                     <span class="my-text-transform-uppercase my-text-green " >available</span>
                 </div>
                 <div class="check-availability
@@ -24,6 +32,7 @@
                             my-justify-content-center 
                             my-align-items-center
                             border-l-1
+                            border-b-1
                 ">
                     <span class="my-text-white" >Check Availability </span>
                 </div>
@@ -31,18 +40,18 @@
             <p class="my-py-1">{{$comic['description']}}</p>
         </div>
 
-        <div class="advertisement">
+        <div class="advertisement my-p-4 ">
              <img src="{{$comic['thumb']}}" alt="">
         </div>
     </div>
 </section>
 <section class="single-cover-info">
-    {{--     <div class="cover-title">
+        <div class="cover-title">
     
         </div>
         <div class="advertisement">
     
-        </div> --}}
+        </div>
     </section>
 
 
